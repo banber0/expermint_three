38d01491c633ab25cc472a92d970fa08f95bf1c7
banber0 <134239676+banber0@users.noreply.github.com>
2023-10-17 11:24:28 +0800

    Add files via upload

---
+++ example/People.java
@@ -0,0 +1,29 @@
+package example;
+public class People {
+    protected int id;
+    protected String name;
+    protected String sex;
+    private int age;
+
+    public People(int id, String name, String sex,int age){
+        this.id = id;
+        this.name = name;
+        this.sex = sex;
+        this.age = age;
+
+    }
+    public String getName(){
+         return name;
+    }
+    protected void liu() {
+        System.out.println("This is a protected method in people.");
+    }
+    void ze() {
+        System.out.println("This is a default method in people.");
+    }
+    private void xu() {
+        System.out.println("This is a private method in people.");
+    }
+}
+       
+    
